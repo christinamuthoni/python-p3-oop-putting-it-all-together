768fc677ed9dd481b6476bc942f46449eea4f306
christine <christine.muriungi@student.moringaschool.com>
2023-12-14 14:52:38 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self._title = title
+        self._page_count = page_count
+
+    def get_page_count(self):
+        print("Retrieving page_count.")
+        return self._page_count
     
+    def get_title(self):
+        print("Retrieving title.")
+        return self._title
+
+    def set_page_count(self, page_count):    
+        if isinstance(page_count, int):
+            print(f"Setting page_count to {page_count}.")
+            self._page_count = page_count
+
+        else:
+            print('page_count must be an integer')
+
+    def set_title(self, title):
+        self._title = title        
+
+    page_count =property(get_page_count, set_page_count) 
+    title = property(get_title, set_title)   
+
+    def turn_page(self):  
+        print("Flipping the page...wow, you read fast!")
         
+
+#my_book = Book("Sample Book", "800")
+#my_book.page_count = "jjj"
+#print(my_book.page_count)    
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,54 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = size
+        self._repaired = False
+        self._condition = 'New'
+
+
+    def get_size(self):
+        print('retrieving size.') 
+        return self._size
+    
+    def get_brand(self):
+        print('retrieving brand.') 
+        return self._brand
+
+    def set_size(self, size):
+        if isinstance(size, int):
+            print(f"setting size to {size}")
+            set._size = size
+
+        else:
+            print("size must be an integer")  
+
+    def set_brand(self, brand):   
+            print(f"setting brand to {brand}")
+            set._brand = brand
+
+    def  repaired (self):
+         return self._repaired  
+    
+    def condition(self):
+        return self._condition  
+
+    def cobble(self):
+        #print(f"The {self._brand} shoe has been repaired.")
+        print('Your shoe is as good as new!')
+        self._repaired = True
+        self._condition = 'New' 
+
+    def is_repaired(self):
+        return self._repaired
+    
+    def get_condition(self):
+        return self._condition       
+    
+
+    size = property(get_size, set_size) 
+    brand = property(get_brand, set_brand)   
+    repaired = property(is_repaired)
+    condition = property(get_condition)          
+        
